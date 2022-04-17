97d5fffcb5538c7a3ff388d36b38ab7ab5181c82
davviiid <92745967+davviiid@users.noreply.github.com>
2022-04-17 22:02:53 +0300

    Add files via upload

---
+++ src/test/java/ro/fasttrackit/project/BudgetCalculator/BudgetCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.project.BudgetCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
